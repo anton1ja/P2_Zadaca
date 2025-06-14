55d12704e8d1b6d4fc3e349fdaede5f1845cf9be
anton1ja <ba.antonija@gmail.com>
2025-06-14 22:49:24 +0200

    Add files via upload

---
+++ rekurzija_zadaca.py
@@ -0,0 +1,12 @@
+#  Napisati rekurzivnu funkciju koja kao parametar prima string, a kao rezultat taj string ispisuje sa zada.
+
+def obrni(s):
+    
+    if s  == s[0]:
+        return s[0]
+    
+    return s[-1] + obrni(s[:-1])
+
+string = input('Unesite string: ')
+
+print('Rezultat: ', obrni(string))
