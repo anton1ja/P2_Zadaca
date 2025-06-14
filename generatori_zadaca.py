0c365bd4aa816edb7983e05fabafa9e147629cba
anton1ja <ba.antonija@gmail.com>
2025-06-14 20:59:39 +0200

    Add files via upload

---
+++ generatori_zadaca.py
@@ -0,0 +1,31 @@
+# Napraviti generator funkcije za ispis svih parnih i svih neparnih brojeva manjih od prosljeđenog parametra.
+
+def parni(maks):
+
+    a = 0
+    
+    while a < maks:
+        yield a
+        a += 2
+
+def neparni(maks):
+
+    b = 1
+
+    while b < maks:
+        yield b
+        b += 2
+
+parametar = int(input('Unesite maksimalnu vrijednost: '))
+
+print(f'Parni brojevi manji od {parametar}:')
+
+parni_gen = parni(parametar)
+for i in parni_gen:
+    print(i)
+
+print(f'Neparni brojevi manji od {parametar}:')
+
+neparni_gen = neparni(parametar)
+for i in neparni_gen:
+    print(i)
